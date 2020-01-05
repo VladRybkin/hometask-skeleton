9daadf0b15323f11ad6363d348eed6fd40288c45
Vlad <RybkinVladyslav@gmail.com>
2020-01-05 23:50:48 +0200

    Auditorium service test

--- src/test/java/ua/training/spring/hometask/service/impl/DefaultAuditoriumServiceTest.java
+++ src/test/java/ua/training/spring/hometask/service/impl/DefaultAuditoriumServiceTest.java
@@ -1,31 +1,55 @@
 package ua.training.spring.hometask.service.impl;
 
 
+import com.google.common.collect.Sets;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
+
+
+import ua.training.spring.hometask.domain.Auditorium;
 import ua.training.spring.hometask.service.AuditoriumService;
 
-import javax.annotation.Resource;
+import java.util.Set;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @ExtendWith(MockitoExtension.class)
-@ContextConfiguration("/auditorium.xml")
 class DefaultAuditoriumServiceTest {
 
-    @Autowired
-    AuditoriumService auditoriumService;
+
+    private AuditoriumService auditoriumService;
+
+    private Set<Auditorium> auditoriums;
+
+    private Auditorium firstAuditorium;
+
+    private Auditorium secondAuditorium;
+
+    private static final String FIRST_AUDITORIUM_NAME = "first";
+
+    private static final String SECOND_AUDITORIUM_NAME = "second";
+
+    @BeforeEach()
+    void setUp() {
+        firstAuditorium = new Auditorium(FIRST_AUDITORIUM_NAME, 60, Sets.newHashSet(1L, 2L));
+        secondAuditorium = new Auditorium(SECOND_AUDITORIUM_NAME, 50, Sets.newHashSet(3L, 4L));
+        auditoriums = Sets.newHashSet(firstAuditorium, secondAuditorium);
+        auditoriumService = new DefaultAuditoriumService(auditoriums);
+    }
 
     @Test
     void getAll() {
-
+        assertThat(auditoriumService.getAll(), containsInAnyOrder(auditoriums.toArray()));
     }
 
     @Test
     void getByName() {
+        assertEquals(auditoriumService.getByName(FIRST_AUDITORIUM_NAME), firstAuditorium);
+        assertEquals(auditoriumService.getByName(SECOND_AUDITORIUM_NAME), secondAuditorium);
     }
 }
