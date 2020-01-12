fb555b28b55e3ae0a9bea706a71345eac90c9ba0
Vlad <RybkinVladyslav@gmail.com>
2020-01-12 18:20:15 +0200

    add provider

--- src/main/java/ua/training/spring/hometask/Main.java
+++ src/main/java/ua/training/spring/hometask/Main.java
@@ -1,7 +1,6 @@
 package ua.training.spring.hometask;
 
 
-
 import org.springframework.shell.Bootstrap;
 
 
@@ -9,6 +8,7 @@ public class Main {
 
 
     public static void main(String[] args) throws Exception {
+
         Bootstrap.main(args);
     }
 
