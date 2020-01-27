e561158e7f948bb2f6e98616a404b331ab972a46
Vladyslav_Rybkin <Footballer0@mail.ru>
2020-01-27 14:00:14 +0200

    remove not necessary dirties context annotation

--- src/test/java/ua/training/spring/hometask/service/impl/DefaultAuditoriumServiceIntegrationTest.java
+++ src/test/java/ua/training/spring/hometask/service/impl/DefaultAuditoriumServiceIntegrationTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
