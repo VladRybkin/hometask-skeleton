866a04ba893a23736a299b61deb89bba37a78f37
Vlad <RybkinVladyslav@gmail.com>
2020-02-06 23:40:57 +0200

    added Hibernate dependency and seesionFactory bean

---
+++ src/main/java/ua/training/spring/hometask/config/BeansHibernate.java
@@ -0,0 +1,27 @@
+package ua.training.spring.hometask.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class BeansHibernate {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public LocalSessionFactoryBean localSessionFactoryBean() {
+        LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
+        localSessionFactoryBean.setDataSource(dataSource);
+        localSessionFactoryBean.setPackagesToScan("ua.training.spring.hometask.domain");
+
+        return localSessionFactoryBean;
+    }
+
+
+
+}
---
+++ src/main/java/ua/training/spring/hometask/dao/impl/hibernate/HibernateUserDaoImpl.java
@@ -0,0 +1,34 @@
+package ua.training.spring.hometask.dao.impl.hibernate;
+
+import ua.training.spring.hometask.dao.UserDao;
+import ua.training.spring.hometask.domain.User;
+
+import java.util.Collection;
+
+public class HibernateUserDaoImpl implements UserDao {
+
+    @Override
+    public User getUserByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public User save(User object) {
+        return null;
+    }
+
+    @Override
+    public void remove(User object) {
+
+    }
+
+    @Override
+    public User getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Collection<User> getAll() {
+        return null;
+    }
+}
