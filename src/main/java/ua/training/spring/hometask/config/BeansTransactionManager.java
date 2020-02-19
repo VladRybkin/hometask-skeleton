a84af3f6c0b42e3bba141328560ac35dfb96edf3
Vlad <RybkinVladyslav@gmail.com>
2020-02-19 20:26:53 +0200

    transaction manager config fix

--- src/main/java/ua/training/spring/hometask/config/BeansTransactionManager.java
+++ src/main/java/ua/training/spring/hometask/config/BeansTransactionManager.java
@@ -2,20 +2,24 @@ package ua.training.spring.hometask.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.stereotype.Component;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
-@Component
+@Configuration
 @EnableTransactionManagement
+@Import(DataSourceBeans.class)
 public class BeansTransactionManager {
 
-    @Bean
     @Autowired
-    public PlatformTransactionManager txManager(DataSource dataSource) {
+    private DataSource dataSource;
+
+    @Bean
+    public PlatformTransactionManager txManager() {
         return new DataSourceTransactionManager(dataSource);
     }
 }
