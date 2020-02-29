d9a74c726316ceba7d1168a6ffe613c80ecf0a5d
Vlad <RybkinVladyslav@gmail.com>
2020-02-29 22:01:22 +0200

    removed jsr annotaions from crud

--- src/main/java/ua/training/spring/hometask/Main.java
+++ src/main/java/ua/training/spring/hometask/Main.java
@@ -1,49 +1,13 @@
 package ua.training.spring.hometask;
 
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.shell.Bootstrap;
-import ua.training.spring.hometask.config.BeansConfiguration;
-import ua.training.spring.hometask.domain.User;
-import ua.training.spring.hometask.service.*;
-
-import java.time.LocalDateTime;
 
 
 public class Main {
 
 
     public static void main(String[] args) throws Exception {
-        //        ApplicationContext applicationContext=new ClassPathXmlApplicationContext("classpath:auditorium.xml", "discount-strategies.xml");
-        //        System.out.println(applicationContext.getId());
-        //        System.out.println(applicationContext.getBean(UserService.class));
-        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
-
-        ctx.register(BeansConfiguration.class);
-
-        ctx.refresh();
-        EventService eventService = ctx.getBean(EventService.class);
-        EventCountService eventCountService = ctx.getBean(EventCountService.class);
-        BookingService bookingService = ctx.getBean(BookingService.class);
-        TicketService ticketService = ctx.getBean(TicketService.class);
-        UserService userService = ctx.getBean(UserService.class);
-        //        User user = new User();
-        //        user.setEmail("brithd");
-        //        user.setFirstName("TestFirst");
-        //        user.setDateOfBirth(LocalDateTime.now().minusDays(5));
-        User user1 = new User();
-        user1.setEmail("birthd3");
-        user1.setFirstName("Testsecond");
-        user1.setDateOfBirth(LocalDateTime.now().plusDays(5));
-
-        UserDiscountCountService defaultEventCountService = ctx.getBean(UserDiscountCountService.class);
-        System.out.println(eventCountService);
-
-        System.out.println(eventService.getAll());
-//        Bootstrap.main(args);
+        Bootstrap.main(args);
     }
-
-
-
-
 }
