8dc73b826a0f764b52ce1437a72d57a13e15a6af
Vlad <RybkinVladyslav@gmail.com>
2020-04-25 22:24:59 +0300

    add MyBatisRepos

--- src/main/java/ua/training/spring/hometask/dao/mybatisrepos/MBUserDiscountCountMapper.java
+++ src/main/java/ua/training/spring/hometask/dao/mybatisrepos/MBUserDiscountCountMapper.java
@@ -1,5 +1,37 @@
 package ua.training.spring.hometask.dao.mybatisrepos;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+import ua.training.spring.hometask.domain.UserDiscountCount;
+
+import java.util.Collection;
+
 public interface MBUserDiscountCountMapper {
 
+    @Insert("INSERT INTO user_discount_counts (name, count_tenth_ticket_discount, count_birthday_discount) "
+            + "VALUES (#{name},#{countTenthTicketDiscount},#{countBirthdayDiscount})")
+    long save(UserDiscountCount object);
+
+    @Delete("DELETE FROM user_discount_counts WHERE id = #{id}")
+    void remove(UserDiscountCount id);
+
+    @Select("SELECT * FROM user_discount_counts WHERE id = #{id}")
+    @Results(value = {
+            @Result(property = "id", column = "id"),
+            @Result(property = "name", column = "name"),
+            @Result(property = "countTenthTicketDiscount", column = "count_tenth_ticket_discount"),
+            @Result(property = "countBirthdayDiscount", column = "count_birthday_discount")})
+    UserDiscountCount getById(@Param("id") Long id);
+
+    @Select("SELECT * FROM event_counts")
+    @Results(value = {
+            @Result(property = "id", column = "id"),
+            @Result(property = "name", column = "name"),
+            @Result(property = "countTenthTicketDiscount", column = "count_tenth_ticket_discount"),
+            @Result(property = "countBirthdayDiscount", column = "count_birthday_discount")})
+    Collection<UserDiscountCount> getAll();
 }
--- src/main/java/ua/training/spring/hometask/dao/mybatisrepos/MBUserMapper.java
+++ src/main/java/ua/training/spring/hometask/dao/mybatisrepos/MBUserMapper.java
@@ -1,5 +1,39 @@
 package ua.training.spring.hometask.dao.mybatisrepos;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+import ua.training.spring.hometask.domain.User;
+
+import java.util.Collection;
+
 public interface MBUserMapper {
 
+    @Insert("INSERT INTO users (first_name, last_name, email, date_of_birth) "
+            + "VALUES (#{firstName},#{lastName},#{email},#{dateOfBirth})")
+    long save(User object);
+
+    @Delete("DELETE FROM users WHERE id = #{id}")
+    void remove(User id);
+
+    @Select("SELECT * FROM event_counts WHERE id = #{id}")
+    @Results(value = {
+            @Result(property = "id", column = "id"),
+            @Result(property = "firstName", column = "first_name"),
+            @Result(property = "lastName", column = "last_name"),
+            @Result(property = "email", column = "email"),
+            @Result(property = "dateOfBirth", column = "date_of_birth")})
+    User getById(@Param("id") Long id);
+
+    @Select("SELECT * FROM users")
+    @Results(value = {
+            @Result(property = "id", column = "id"),
+            @Result(property = "firstName", column = "first_name"),
+            @Result(property = "lastName", column = "last_name"),
+            @Result(property = "email", column = "email"),
+            @Result(property = "dateOfBirth", column = "date_of_birth")})
+    Collection<User> getAll();
 }
