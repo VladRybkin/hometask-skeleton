66c379edfef2370d10d15ff9525ea64da2f37fec
Vlad <RybkinVladyslav@gmail.com>
2020-05-02 23:45:25 +0300

    added event MyBatis repos tests

--- src/main/java/ua/training/spring/hometask/config/BeansTransactionManager.java
+++ src/main/java/ua/training/spring/hometask/config/BeansTransactionManager.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -14,6 +15,7 @@ import javax.sql.DataSource;
 @Configuration
 @EnableTransactionManagement
 @Import(DataSourceBeans.class)
+@Profile({"MYBATIS", "JDBC_TEMPLATE"})
 public class BeansTransactionManager {
 
     @Autowired
