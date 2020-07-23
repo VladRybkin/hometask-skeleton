5e194396823fdff28704a48f1b1d505b55feee16
vlad <Footballer0@mail.ru>
2020-07-23 22:52:21 +0300

    add spring mvc config

--- src/main/java/ua/training/spring/hometask/config/DispatcherServletInitializer.java
+++ src/main/java/ua/training/spring/hometask/config/DispatcherServletInitializer.java
@@ -3,21 +3,30 @@ package ua.training.spring.hometask.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
 @Configuration
 public class DispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return null;
+        return new Class<?>[]{BeansConfiguration.class};
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class<?>[]{BeansConfiguration.class};
+        return null;
     }
 
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        servletContext.setInitParameter("spring.profiles.active", "HIBERNATE");
     }
 }
