0813f524e2169e7751a4f71391f68c90289f36fe
Vlad <RybkinVladyslav@gmail.com>
2021-02-06 18:15:33 +0200

    added rest exception handler

---
+++ src/main/java/ua/training/spring/hometask/Main.java
@@ -0,0 +1,10 @@
+package ua.training.spring.hometask;
+
+import org.springframework.shell.Bootstrap;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Bootstrap.main(args);
+    }
+}
